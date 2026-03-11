6baa0f9185499df5b316c47ed775c5b728467d73
Victorwenney <251069272+Victorwenney@users.noreply.github.com>
2026-03-10 22:07:28 -0300

    Change from a 16 square grip to a 16x16 grid Add color change on mouse hover
    Changed the grid from a simples 16 squares grid to a bigger grid with
    16x16 squares. Used flex-basis % to determine the grid square size.
    Added the event listener to change the square color to black when the
    mouse cursor enters it's div. Alse changed the body background color.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const gridContainer = document.querySelector('#gridContainer');
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < 16*16; i++){
     const div = document.createElement('div');
-    div.setAttribute("style", "width: 100px; aspect-ratio: 1 / 1; background-color: white;");
+    div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: white; flex: 0 0 6.25%;");
+    div.addEventListener("mouseenter", () => {
+        div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: black; flex: 0 0 6.25%;");
+    })
     gridContainer.appendChild(div);
 }
