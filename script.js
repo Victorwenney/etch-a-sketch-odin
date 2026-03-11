86f4ebf19a300412297f3ec4591f6ebb7d97a2f1
Victorwenney <251069272+Victorwenney@users.noreply.github.com>
2026-03-11 07:48:57 -0300

    Add Rainbow Button to htlm file Create new function no generate random rgb color Create logic to paint grid by random color
    Added a new button, Rainbow Button, to the html file.
    Created a new function that generates a random rgb color and returns it
    as a string.
    Created the logic that, upon clicking the Rainbow Button, changes the
    grid attributes to painting by random colors instead of black.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const gridContainer = document.querySelector('#gridContainer');
 
 for(let i = 0; i < 16*16; i++){
     const div = document.createElement('div');
+    div.classList.add('pixel');
     div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: white; flex: 0 0 6.25%;");
     div.addEventListener("mouseenter", () => {
         div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: black; flex: 0 0 6.25%;");
@@ -18,10 +19,28 @@ sizeButton.addEventListener('click', () => {
     gridContainer.replaceChildren();
     for(let i = 0; i < size*size; i++){
         const div = document.createElement('div');
+        div.classList.add('pixel');
         div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: white; flex: 0 0 " + 100 / size + "%;");
         div.addEventListener("mouseenter", () => {
             div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: black; flex: 0 0 " + 100 / size + "%;");
         });
         gridContainer.appendChild(div);
     }
+});
+
+function randomRgbColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const rainbowButton = document.querySelector('.rainbowButton');
+rainbowButton.addEventListener('click', () => {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mouseenter", () => {
+            pixel.style.backgroundColor = randomRgbColor();
+        });
+    });
 });
