c7641b7b2400995884a8f176ed22529be9dd5d57
Victorwenney <251069272+Victorwenney@users.noreply.github.com>
2026-03-11 06:48:18 -0300

    Add button to change grid size in the html file Add style to the new button Add logic to changing the grid size in JavaScript
    Added a button, named Grid Size, in the html file to change grid size at
    will.
    Added CSS tot the button to change it's chape and apparence.
    Added JavaScript code to prompt the user when the button is clicked and
    change the grid size according to the user input.

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const gridContainer = document.querySelector('#gridContainer');
+
 for(let i = 0; i < 16*16; i++){
     const div = document.createElement('div');
     div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: white; flex: 0 0 6.25%;");
     div.addEventListener("mouseenter", () => {
         div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: black; flex: 0 0 6.25%;");
-    })
+    });
     gridContainer.appendChild(div);
 }
+
+const sizeButton = document.querySelector('.sizeButton');
+sizeButton.addEventListener('click', () => {
+    let size = prompt("Plase enter the grid size tou desire:");
+    if(size > 100){
+        size = 100;
+    }
+    gridContainer.replaceChildren();
+    for(let i = 0; i < size*size; i++){
+        const div = document.createElement('div');
+        div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: white; flex: 0 0 " + 100 / size + "%;");
+        div.addEventListener("mouseenter", () => {
+            div.setAttribute("style", "aspect-ratio: 1 / 1; background-color: black; flex: 0 0 " + 100 / size + "%;");
+        });
+        gridContainer.appendChild(div);
+    }
+});
